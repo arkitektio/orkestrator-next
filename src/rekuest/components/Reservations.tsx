33e1b6d01cd54e6523c8e0a3bc1c56fe1b5be0b6
jhnnsrs <jhnnsrs@gmail.com>
2023-09-07 20:12:03 +0200

    New is always better

---
+++ src/rekuest/components/Reservations.tsx
@@ -0,0 +1,41 @@
+import { useSettings } from "@/providers/settings/SettingsContext";
+import { withRekuest } from "@jhnnsrs/rekuest-next";
+import React from "react";
+import { useReservationsQuery } from "../api/graphql";
+import { useRequestMate } from "@/mates/request/useRequestMate";
+import { RekuestReservation } from "@/linkers";
+import { ContainerGrid } from "@/components/layout/ContainerGrid";
+
+export type IMyReservationsProps = {};
+
+const Reservations: React.FC<IMyReservationsProps> = () => {
+  const { settings } = useSettings();
+  const { data } = withRekuest(useReservationsQuery)({
+    fetchPolicy: "cache-and-network",
+    variables: {
+      instanceId: settings.instanceId,
+    },
+  });
+  const requesterMate = useRequestMate();
+
+  return (
+    <>
+      <RekuestReservation.ListLink className="font-light text-xl dark:text-white">
+        This app uses
+      </RekuestReservation.ListLink>
+      <div className="mt-2 mb-4">
+        <ContainerGrid>
+          {data?.reservations?.filter(notEmpty).map((res, index) => (
+            <ReservationCard
+              key={index}
+              reservation={res}
+              mates={[requesterMate(res)]}
+            />
+          ))}
+        </ContainerGrid>
+      </div>
+    </>
+  );
+};
+
+export { Reservations };

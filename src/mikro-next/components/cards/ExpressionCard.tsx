f29371defae4162f3ba3c90655c58f0ac28c2ae9
jhnnsrs <jhnnsrs@gmail.com>
2024-09-16 22:03:25 +0200

    add stuff

--- src/mikro-next/components/cards/ExpressionCard.tsx
+++ src/mikro-next/components/cards/ExpressionCard.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/ui/card";
+import { Image } from "@/components/ui/image";
 import { useResolve } from "@/datalayer/hooks/useResolve";
 import { MikroExpression } from "@/linkers";
 import { MateFinder } from "@/mates/types";
@@ -14,17 +15,26 @@ const TheCard = ({ item, mates }: Props) => {
 
   return (
     <MikroExpression.Smart object={item?.id} mates={mates}>
-      <Card className="px-2 py-2 h-20 transition-all ease-in-out duration-200 truncate">
-        <MikroExpression.DetailLink
-          className={({ isActive } /*  */) =>
-            "z-10 font-bold text-md mb-2 cursor-pointer " +
-            (isActive ? "text-primary-300" : "")
-          }
-          object={item.id}
-        >
-          {item?.label}
-        </MikroExpression.DetailLink>
-        <p className="text-sm text-muted-foreground">{item?.description}</p>
+      <Card className="px-2 py-2 h-20 transition-all ease-in-out duration-200 truncate relative">
+        {item?.store?.presignedUrl && (
+          <Image
+            src={s3resolve(item?.store?.presignedUrl)}
+            style={{ filter: "brightness(0.2)" }}
+            className="z-3 object-cover h-full w-full absolute top-0 left-0 rounded rounded-lg"
+          />
+        )}
+        <div className="p-3 h-full w-full absolute top-0 left-0 bg-opacity-20  hover:bg-opacity-10 transition-all ease-in-out duration-200 flex flex-col break-all overflow-y-hidden">
+          <MikroExpression.DetailLink
+            className={({ isActive } /*  */) =>
+              "z-10 font-bold text-md mb-2 cursor-pointer " +
+              (isActive ? "text-primary-300" : "")
+            }
+            object={item.id}
+          >
+            {item?.label}
+          </MikroExpression.DetailLink>
+          <p className="text-sm text-muted-foreground">{item?.description}</p>
+        </div>
       </Card>
     </MikroExpression.Smart>
   );
--- src/mikro-next/components/cards/ImageCard.tsx
+++ src/mikro-next/components/cards/ImageCard.tsx
@@ -17,7 +17,7 @@ const ImageCard = ({ image, mates }: ImageCardProps) => {
     <MikroImage.Smart
       object={image?.id}
       dragClassName={({ isOver, canDrop, isSelected, isDragging }) =>
-        `relative rounded group text-white bg-center bg-background shadow-lg h-60 rounded roounded-lg hover:bg-back-800 transition-all ease-in-out duration-200 group ${
+        `relative rounded group text-white bg-center bg-background shadow-lg aspect-square rounded rounded-lg hover:bg-back-800 transition-all ease-in-out duration-200 group ${
           isOver && !isDragging && "border-primary-200 border"
         } ${isDragging && "ring-primary-200 ring"} ${
           isSelected && "ring-2 ring-secondary-500"
@@ -33,7 +33,7 @@ const ImageCard = ({ image, mates }: ImageCardProps) => {
         />
       )}
       <div
-        className="px-2 py-2 h-full w-full absolute rounded-lg rounded  top-0 left-0 bg-opacity-20  hover:bg-opacity-10 transition-all ease-in-out duration-200 flex flex-row break-all"
+        className="px-2 py-2 h-full w-full absolute rounded-lg rounded  top-0 left-0 bg-opacity-20  hover:bg-opacity-10 transition-all ease-in-out duration-200 flex flex-row break-all overflow-y-hidden"
         style={{
           backgroundSize: `${progress || 0}% 100%`,
           backgroundImage: `linear-gradient(to right, #10b981 ${progress}%, #10b981 ${progress}%)`,

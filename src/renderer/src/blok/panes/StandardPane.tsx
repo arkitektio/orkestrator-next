671c5bf26757ed53d5ee4cc5bb270c808d034f08
jhnnsrs <jhnnsrs@gmail.com>
2026-05-22 20:54:25 +0200

    fix: wrong imports

--- src/renderer/src/blok/panes/StandardPane.tsx
+++ src/renderer/src/blok/panes/StandardPane.tsx
@@ -1,11 +1,6 @@
-import { SidebarLayout } from "@/components/layout/SidebarLayout";
-import { Card } from "@/components/ui/card";
 import { DroppableNavLink } from "@/components/ui/link";
-import { BlokBlok } from "@/linkers";
 import { Home, Image } from "lucide-react";
-import React from "react";
-import GlobalSearchFilter from "../filters/GlobalSearchFilter";
-import registry, { Registration } from "../registry";
+import { exp } from "three/src/nodes/math/MathNode.js";
 
 interface IDataSidebarProps { }
 
@@ -15,7 +10,7 @@ export const NavigationPane = (props: {}) => {
       <nav className="grid items-start px-1 text-sm font-medium lg:px-2">
         <div className="text-muted-foreground text-xs font-semibold uppercase mb-4">
           Explore
-        </div>
+        </div>̉
         <div className="flex flex-col items-start gap-4 rounded-lg ml-2 text-muted-foreground mb-4">
           <DroppableNavLink
             to="/blok"
@@ -50,74 +45,4 @@ export const NavigationPane = (props: {}) => {
   );
 };
 
-export const modules = (search?: string) => {
-  return [
-    ...registry.components
-      .keys()
-      .map((key) => {
-        if (search) {
-          if (key.includes(search)) {
-            return registry.components.get(key);
-          }
-        }
-        return registry.components.get(key);
-      })
-      .filter((x) => x),
-  ];
-};
-
-export const FilteredBlokCard = (props: { app: Registration }) => {
-  return (
-    <BlokBlok.Smart object={props.app.name}>
-      <Card className="flex flex-col gap-2 p-2 bg-gray-800 rounded-lg h-20">
-        <BlokBlok.DetailLink object={props.app.name}>
-          <div className="flex flex-row items-center gap-2">
-            <h3 className="text-lg font-semibold">{props.app.name}</h3>
-          </div>
-        </BlokBlok.DetailLink>
-      </Card>
-    </BlokBlok.Smart>
-  );
-};
-
-const defaultVariables = { search: undefined };
-
-const Pane: React.FunctionComponent<IDataSidebarProps> = (props) => {
-  const [filteredModules, setFilteredModules] = React.useState(modules());
-
-  const [currentVariables, setCurrentVariables] = React.useState<{
-    search?: string;
-  }>(defaultVariables);
-
-  const onFilterChanged = (e: { search?: string }) => {
-    setFilteredModules(modules(e.search));
-    setCurrentVariables(e);
-  };
-
-  return (
-    <>
-      <SidebarLayout
-        searchBar={
-          <GlobalSearchFilter
-            onFilterChanged={onFilterChanged}
-            defaultValue={defaultVariables}
-          />
-        }
-      >
-        {!currentVariables?.search || currentVariables?.search == "" ? (
-          <>
-            <NavigationPane />
-          </>
-        ) : (
-          <div className="flex flex-col gap-3">
-            {filteredModules.map((item, i) => (
-              <FilteredBlokCard app={item} key={i} />
-            ))}
-          </div>
-        )}
-      </SidebarLayout>
-    </>
-  );
-};
-
-export default Pane;
+export default NavigationPane;
